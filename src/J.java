1aa08921d16f93708d4b58771ed05ddde76b5d94
Lukas <git@lukasv.de>
2019-10-25 14:15:09 +0200

    Added more methods

--- src/J.java
+++ src/J.java
@@ -3,42 +3,56 @@ public class J {  // class for complex numbers
   private float r;  // real part
   private float j;  // imaginary part
 	
-  J(float r, float j) { // constructor with real and imaginary part
+  public J(float r, float j) { // constructor with real and imaginary part
 	  this.r = r;
 	  this.j = j;
   }
   
-  float r() {  // return real part of J. chose r() instead of getR() for shorter code
+  public float r() {  // return real part of J. chose r() instead of getR() for shorter code
 	  return this.r;
   }
   
-  J r(float r) {
+  public J r(float r) {
 	  this.r = r;
 	  return this;
   }
   
-  float j() {  // return imaginary part of J. chose i() instead of getI() for shorter code
+  public float j() {  // return imaginary part of J. chose i() instead of getI() for shorter code
 	  return this.j;
   }
   
-  J j(float j) {
+  public J j(float j) {
 	  this.j = j;
 	  return this;
   }
   
-  void print() {
+  public void print() {
 	  System.out.println("(" + String.valueOf(this.r) + " + " + String.valueOf(this.j) + "j)");
   }
   
-  boolean equals(J j) {
+  public boolean equals(J j) {
 	  return this.r == j.r() && this.j == j.j();
   }
   
-  J plus(J j) {  // returns addition of J with a second J
+  public J plus(J j) {  // returns addition of J with a second J
 	  return new J(this.r + j.r(), this.j + j.j());
   }
   
-  J minus(J j) {  // returns subtraction of J with a second J
+  public J minus(J j) {  // returns subtraction of J with a second J
 	  return new J(this.r - j.r(), this.j - j.j());
+  }
+  
+  public J mult(J j) {  // returns multiplication of J with a second J
+	  return new J(this.r * j.r() - this.j * j.j(), this.r * j.j() + this.j * j.r());
+  }
+  
+  public J div(J j) {  // returns division of J with a second J
+	  J numerator = this.mult(j.conj());
+	  float denominator = j.mult(j.conj()).r();
+	  return new J(numerator.r() / denominator, numerator.j() / denominator);
+  }
+  
+  public J conj() {  // returns conjugate of J with a second J
+	  return new J(this.r, -this.j);
   }
 }
--- src/JTest.java
+++ src/JTest.java
@@ -18,6 +18,12 @@ class JTest {
 		assertFalse(a.equals(b));
 		assertTrue(a.plus(b).equals(new J(8, 8)));
 		assertTrue(a.minus(b).equals(new J(0, -2)));
+		assertTrue(a.mult(b).equals(new J(1, 32)));
+		b.r(11);
+		b.j(2);
+		b.print();
+		assertTrue(b.div(a).equals(new J(2, -1)));
+		assertTrue(a.conj().equals(new J(4, -3)));
 	}
 
 }
