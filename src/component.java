1a6007795a664d25a04490d5c7999fa36dfdd9ac
aarongedemer <Aaron.Gedemer@web.de>
2019-10-31 10:53:09 +0100

    fixed errors for build

--- src/component.java
+++ src/component.java
@@ -4,14 +4,13 @@ public class component extends J { //class representing a component in a electro
     private float value; // value of the component
 
     public component(float im, float re, float freq, float value){
-        this.r = re;
-        this.j = im;
+        super(im,re);
         this.freq = freq;
         this.value = value;
     }
 
-    public void setFreq(float toSet){this.freq = toSet};
-    public float getFreq() {return this.freq};
-    public void setValue(float toSet){this.value = toSet};
-    public float getValue(){return this.value};
+    public void setFreq(float toSet){this.freq = toSet;};
+    public float getFreq() {return this.freq;};
+    public void setValue(float toSet){this.value = toSet;};
+    public float getValue(){return this.value;};
 }
