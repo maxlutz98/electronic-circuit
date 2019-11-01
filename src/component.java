c300ad43f3a74395671c68d33353c159a55ff0bf
aarongedemer <Aaron.Gedemer@web.de>
2019-11-01 11:53:37 +0100

    added constuctors

--- src/component.java
+++ src/component.java
@@ -3,11 +3,20 @@ public class component extends J { //class representing a component in a electro
     private float freq; // freqency applied to the curcuit
     private float value; // value of the component
 
+    public component(){
+        this.freq = 0;
+        this.value = 0;
+    }
+    public component(float freq, float value){
+        this.freq = freq;
+        this.value = value;
+    }
     public component(float im, float re, float freq, float value){
         super(im,re);
         this.freq = freq;
         this.value = value;
     }
+
 
     public void setFreq(float toSet){this.freq = toSet;};
     public float getFreq() {return this.freq;};
