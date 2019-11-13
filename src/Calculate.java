1a2d6fc9761c5e52fa20297a18efaa8720102b95
Maximilian <max.lutz98@me.com>
2019-11-13 14:40:06 +0100

    Add attributes and constructor

--- src/Calculate.java
+++ src/Calculate.java
@@ -1,2 +1,15 @@
+import java.util.ArrayList;
+
 public class Calculate {
+    private double frequency;
+    private ArrayList resistors;
+    private ArrayList condensators;
+    private ArrayList inductors;
+
+    public Calculate(double frequency, ArrayList resistors, ArrayList condensators, ArrayList inductors) {
+        this.frequency = frequency;
+        this.resistors = resistors;
+        this.condensators = condensators;
+        this.inductors = inductors;
+    }
 }
