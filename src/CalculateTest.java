1f8ef6e8b3d9616af6a3c59f0fb82312f4234d6c
Maximilian <max.lutz98@me.com>
2019-11-19 11:23:18 +0100

    Add easy test function

--- src/CalculateTest.java
+++ src/CalculateTest.java
@@ -1,3 +1,26 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
 class CalculateTest {
+
+    @Test
+    void testCalculate() {
+        ArrayList<Double> parameter1 = new ArrayList<Double>();
+        ArrayList<Double> parameter2 = new ArrayList<Double>();
+        ArrayList<Double> parameter3 = new ArrayList<Double>();
+
+        parameter1.add(10.2);
+        parameter1.add(329.2);
+
+        parameter2.add(25.10);
+
+        parameter3.add(0.25);
+        parameter3.add(5.20);
+
+        Calculate calc = new Calculate(100.5, parameter1, parameter2, parameter3);
+        Circuit result = calc.calculate("(R1+L1)+C1");
+        result.print();
+    }
 
 }
