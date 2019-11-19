81fe0cc0220beb0092da80bbcf9b390ebc7e2f53
Maximilian <max.lutz98@me.com>
2019-11-19 15:21:04 +0100

    Implement test method for calculateImpedance method with two parameter

--- src/ParalellTest.java
+++ src/ParalellTest.java
@@ -6,6 +6,17 @@ class ParalellTest {
 
     @Test
     void calculateImpedance() {
+        Circuit circ1 = new Circuit("Circ1");
+        circ1.setIm(15.0);
+        circ1.setRe(10.0);
+        Circuit circ2 = new Circuit("Circ2");
+        circ2.setIm(22.5);
+        circ2.setRe(5.0);
+
+        Paralell par = new Paralell("Test");
+        par.calculateImpedance(circ1, circ2);
+        assertEquals(par.getRe(), 4.25287356321839);
+        assertEquals(par.getIm(), 9.36781609195402);
     }
 
     @Test
