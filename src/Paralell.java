53bc60af65517e9c026043f528d9bfa4ae74cbce
Maximilian <max.lutz98@me.com>
2019-11-19 10:25:03 +0100

    Implement constructors

--- src/Paralell.java
+++ src/Paralell.java
@@ -1,5 +1,21 @@
 public class Paralell extends Circuit {
 
+    public Paralell(Circuit component1, Circuit component2) {
+        super(component1, component2);
+    }
+
+    public Paralell(Circuit component1, Circuit component2, String formula) {
+        super(component1, component2, formula);
+    }
+
+    public Paralell(String formula) {
+        super(formula);
+    }
+
+    public Paralell(double re, double im, Circuit component1, Circuit component2, String formula) {
+        super(re, im, component1, component2, formula);
+    }
+
     public void calculateImpedance(Circuit part1, Circuit part2){
         Complex additivity1 = part1.inv();
         Complex additivity2 = part2.inv();
--- src/Seriell.java
+++ src/Seriell.java
@@ -1,5 +1,21 @@
 public class Seriell extends Circuit{
 
+    public Seriell(Circuit component1, Circuit component2) {
+        super(component1, component2);
+    }
+
+    public Seriell(Circuit component1, Circuit component2, String formula) {
+        super(component1, component2, formula);
+    }
+
+    public Seriell(String formula) {
+        super(formula);
+    }
+
+    public Seriell(double re, double im, Circuit component1, Circuit component2, String formula) {
+        super(re, im, component1, component2, formula);
+    }
+
     public void calculateImpedance(Circuit part1, Circuit part2){
         Complex impedance = part1.plus(part2);
         this.setRe(impedance.getRe());
