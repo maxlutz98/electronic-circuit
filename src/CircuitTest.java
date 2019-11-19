8492815af4a08ca620688d8365ff8d36e9112160
Maximilian <max.lutz98@me.com>
2019-11-19 12:51:55 +0100

    Added test file for Circuit class

---
+++ src/CircuitTest.java
@@ -0,0 +1,40 @@
+import org.junit.jupiter.api.Test;
+
+class CircuitTest {
+
+    @Test
+    void getComponent1() {
+    }
+
+    @Test
+    void getComponent2() {
+    }
+
+    @Test
+    void setComponent1() {
+    }
+
+    @Test
+    void setComponent2() {
+    }
+
+    @Test
+    void getFormula() {
+    }
+
+    @Test
+    void setFormula() {
+    }
+
+    @Test
+    void calculateImpedance() {
+    }
+
+    @Test
+    void testCalculateImpedance() {
+    }
+
+    @Test
+    void testCalculateImpedance1() {
+    }
+}
