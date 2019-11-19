d6f840ef5245aa02f3da14042b6edf39939c0d8f
Maximilian <max.lutz98@me.com>
2019-11-19 10:08:32 +0100

    Changed parameter of calculateImpedance to one Component

--- src/CircuitImpedance.java
+++ src/CircuitImpedance.java
@@ -1,7 +1,9 @@
 public interface CircuitImpedance {
 
-    public void calculateImpedance(Component part1, Component part2);
-    public void calculateImpedance(Circuit part1, Circuit part2);
-    public void calculateImpedance();
+    void calculateImpedance(Component part1);
+
+    void calculateImpedance(Circuit part1, Circuit part2);
+
+    void calculateImpedance();
 
 }
