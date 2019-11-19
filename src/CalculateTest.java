b4b69256bcb90a0d63f475dddffd433981358a61
Maximilian <max.lutz98@me.com>
2019-11-19 17:13:46 +0100

    Implement more complicated test for calculate method

--- src/CalculateTest.java
+++ src/CalculateTest.java
@@ -2,6 +2,8 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class CalculateTest {
 
     @Test
@@ -20,7 +22,12 @@ class CalculateTest {
         parameter3.add(5.20);
 
         Calculate calc = new Calculate(100.5, parameter1, parameter2, parameter3);
-        Circuit result = calc.calculate("(R1+L1)+C1");
-        result.print();
+        Circuit result = calc.calculate("(R1+L1)||C1");
+        assertEquals(result.getRe(), 1.622482464960298E-12);
+        assertEquals(result.getIm(), -6.309290457508656E-5);
+
+        result = calc.calculate("R1||C1||L1");
+        assertEquals(result.getRe(), 3.902661465916038E-10);
+        assertEquals(result.getIm(), -6.309290467750467E-5);
     }
 }
