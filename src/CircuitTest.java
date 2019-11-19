143a5ae3008d817b82ff59db3c50a86d43a9a548
Maximilian <max.lutz98@me.com>
2019-11-19 13:42:04 +0100

    Implement test for getComponent1 method

--- src/CircuitTest.java
+++ src/CircuitTest.java
@@ -1,9 +1,15 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 class CircuitTest {
 
     @Test
     void getComponent1() {
+        Circuit circ1 = new Circuit("Circ1");
+        Circuit circ2 = new Circuit("Circ2");
+        Circuit test = new Circuit(circ1, circ2, "Main");
+        assertTrue(test.getComponent1() == circ1);
     }
 
     @Test
