2806d393875da02f54870d3d0f56f4e9a77b5de5
Maximilian <max.lutz98@me.com>
2019-11-19 10:14:03 +0100

    Deleted calculateImpedance method with multiple Components

--- src/Paralell.java
+++ src/Paralell.java
@@ -1,13 +1,5 @@
 public class Paralell extends Circuit {
 
-    public void calculateImpedance(Component part1, Component part2){
-        Complex additivity1 = part1.inv();
-        Complex additivity2 = part2.inv();
-        additivity1 =  additivity2.plus(additivity1);
-        additivity1 = additivity1.inv();
-        this.setIm(additivity1.getIm());
-        this.setRe(additivity1.getRe());
-    };
     public void calculateImpedance(Circuit part1, Circuit part2){
         Complex additivity1 = part1.inv();
         Complex additivity2 = part2.inv();
@@ -15,7 +7,8 @@ public class Paralell extends Circuit {
         additivity1 = additivity1.inv();
         this.setIm(additivity1.getIm());
         this.setRe(additivity1.getRe());
-    };
+    }
+
     public void calculateImpedance(){
         Complex additivity1 = this.getComponent1().inv();
         Complex additivity2 = this.getComponent2().inv();
