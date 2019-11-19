c829e12307095774e3a1225cbd21aa59c715d6c0
Maximilian <max.lutz98@me.com>
2019-11-19 12:47:28 +0100

    Added default test for calculateImpedance method

--- src/ParalellTest.java
+++ src/ParalellTest.java
@@ -8,5 +8,15 @@ class ParalellTest {
 
     @Test
     void testCalculateImpedance() {
+        Circuit circ1 = new Circuit("Circ1");
+        circ1.setIm(15.0);
+        circ1.setRe(10.0);
+        Circuit circ2 = new Circuit("Circ2");
+        circ2.setIm(22.5);
+        circ2.setRe(5.0);
+
+        Paralell par = new Paralell(circ1, circ2);
+        par.calculateImpedance();
+        par.print();
     }
 }
--- src/SeriellTest.java
+++ src/SeriellTest.java
@@ -8,5 +8,15 @@ class SeriellTest {
 
     @Test
     void testCalculateImpedance() {
+        Circuit circ1 = new Circuit("Circ1");
+        circ1.setIm(15.0);
+        circ1.setRe(10.0);
+        Circuit circ2 = new Circuit("Circ2");
+        circ2.setIm(22.5);
+        circ2.setRe(5.0);
+
+        Seriell ser = new Seriell(circ1, circ2);
+        ser.calculateImpedance();
+        ser.print();
     }
 }
