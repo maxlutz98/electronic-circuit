0ec78a807a466bc4b871791d02d81872f1dbc802
Maximilian <max.lutz98@me.com>
2019-11-20 10:15:21 +0100

    Added comments to main class

--- src/app.java
+++ src/app.java
@@ -2,7 +2,9 @@
 public class app {
 
 	public static void main(String[] args) {
+        // initialise gui object
         Gui gui = new Gui();
+        // create main frame
         gui.init();
 	}
 
